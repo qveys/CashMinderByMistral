d80ec6193116896e12ef485987f7f19656801df3
Quentin Veys <quentinveys@gmail.com>
2025-09-30 20:37:44 +0200

    Met à jour HomeScreen pour afficher le graphique et la liste des transactions

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/transaction_list.dart';
+import '../widgets/pie_chart.dart';
 import '../../bloc/transaction_bloc.dart';
 import 'import_screen.dart';
 
@@ -18,7 +19,25 @@ class HomeScreen extends StatelessWidget {
           if (state is TransactionLoading) {
             return const Center(child: CircularProgressIndicator());
           } else if (state is TransactionLoaded) {
-            return TransactionList(transactions: state.transactions);
+            return Column(
+              children: [
+                const SizedBox(height: 20),
+                const Text(
+                  'Répartition des dépenses par catégorie :',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                PieChartWidget(transactions: state.transactions),
+                const SizedBox(height: 20),
+                const Text(
+                  'Liste des transactions :',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  child: TransactionList(transactions: state.transactions),
+                ),
+              ],
+            );
           } else if (state is TransactionError) {
             return Center(child: Text('Erreur : ${state.message}'));
           } else {
