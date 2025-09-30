5039f4117b93636865c975ffc1296f6bc8cd779e
Quentin Veys <quentinveys@gmail.com>
2025-09-30 20:20:56 +0200

    Met à jour main.dart pour configurer BlocProvider et RepositoryProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/screens/home_screen.dart';
+import 'bloc/transaction_bloc.dart';
+import 'data/repositories/csv_repository.dart';
+import 'data/repositories/classification_repository.dart';
+import 'data/repositories/local_storage_repository.dart';
 
 void main() {
   runApp(const CashMinderApp());
@@ -10,12 +15,24 @@ class CashMinderApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CashMinder',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(create: (context) => CsvRepository()),
+        RepositoryProvider(create: (context) => ClassificationRepository()),
+        RepositoryProvider(create: (context) => LocalStorageRepository()),
+      ],
+      child: BlocProvider(
+        create: (context) => TransactionBloc(
+          csvRepository: RepositoryProvider.of<CsvRepository>(context),
+          classificationRepository: RepositoryProvider.of<ClassificationRepository>(context),
+          localStorageRepository: RepositoryProvider.of<LocalStorageRepository>(context),
+        )..add(LoadTransactions()),
+        child: MaterialApp(
+          title: 'CashMinder',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: const HomeScreen(),
+        ),
       ),
-      home: const HomeScreen(),
     );
   }
 }
