94d9837b38af5c15361c4a225200f3515fd91550
Quentin Veys <quentinveys@gmail.com>
2025-09-30 20:06:35 +0200

    Corrige l'import de ImportScreen et supprime la classe obsolète

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'import_screen.dart'; // Import du fichier séparé
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -20,22 +21,6 @@ class HomeScreen extends StatelessWidget {
           );
         },
         child: const Icon(Icons.upload_file),
-      ),
-    );
-  }
-}
-
-class ImportScreen extends StatelessWidget {
-  const ImportScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Importer un relevé'),
-      ),
-      body: const Center(
-        child: Text('Écran d\'importation des relevés bancaires'),
       ),
     );
   }
