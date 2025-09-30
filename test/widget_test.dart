23ec496d8e089469cd0a04fee33bbf034d08049d
qveys <contact@quentinveys.be>
2025-09-30 20:11:15 +0200

    Met à jour les tests unitaires pour CashMinder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:cashminder/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('CashMinder app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CashMinderApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our app shows the correct title and content.
+    expect(find.text('CashMinder'), findsOneWidget);
+    expect(find.text('Aperçu des dépenses récentes'), findsOneWidget);
+    expect(find.byIcon(Icons.upload_file), findsOneWidget);
   });
 }
